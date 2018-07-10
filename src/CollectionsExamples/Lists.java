5158faccbb84968d2f425aa678e7c4f45b3109b7
pedroguerzonidefigueiredo <pedroguerzonidefigueiredo@192.168.50.228>
2018-07-09 22:00:19 -0300

    a

--- src/CollectionsExamples/Lists.java
+++ src/CollectionsExamples/Lists.java
@@ -6,7 +6,7 @@ public class Lists {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// lallala
-		
+		//uehauiheaiu
 		HashMap<Integer,String> hm = new HashMap<Integer,String>(); 
 		
 		//test1
